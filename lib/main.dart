a11bfb6b0ab7a11e60f62e12fc0dffe3e4e03211
Jorge <wiijlg@hotmail.com>
2021-03-21 22:50:34 +0100

    Provider gestor de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/home_page.dart';
 import 'package:qr_reader/pages/map_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'QR Reader',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomePage(),
-        'map': (_) => MapPage(),
-      },
-      theme: ThemeData(
-          primaryColor: Colors.teal,
-          floatingActionButtonTheme:
-              FloatingActionButtonThemeData(backgroundColor: Colors.teal)),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => new UiProvider())],
+      child: MaterialApp(
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomePage(),
+          'map': (_) => MapPage(),
+        },
+        theme: ThemeData(
+            primaryColor: Colors.teal,
+            floatingActionButtonTheme:
+                FloatingActionButtonThemeData(backgroundColor: Colors.teal)),
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/listDirections_page%20copy.dart';
 import 'package:qr_reader/pages/listMap_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigationbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -26,7 +28,9 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     switch (currentIndex) {
       case 0:
