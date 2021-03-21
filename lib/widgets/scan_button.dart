16bbfb897512371ce84ace6ddf7f0837b9124aac
Jorge López Gil <wiijlg@hotmail.com>
2021-03-22 00:18:55 +0100

    Escaneo

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', true, ScanMode.QR);
+        print(barcodeScanRes);
+      },
       elevation: 0.0,
       child: Icon(Icons.filter_center_focus_sharp),
     );
