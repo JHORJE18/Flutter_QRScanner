ba37dccd94c35f5bc213f582514ad446cda5b8ab
Jorge <wiijlg@hotmail.com>
2021-03-21 20:24:33 +0100

    Diseño app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigationbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Page')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_outline), onPressed: () {})
+        ],
+      ),
+      body: Center(child: Text('Home Page')),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
---
+++ lib/widgets/scan_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScanButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      elevation: 0.0,
+      child: Icon(Icons.filter_center_focus_sharp),
+    );
+  }
+}
