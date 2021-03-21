9d3ceae0ccc0c04a4534b6060fec3324d7739fcd
Jorge <wiijlg@hotmail.com>
2021-03-21 23:39:01 +0100

    Cambio menu

--- lib/widgets/custom_navigationbar.dart
+++ lib/widgets/custom_navigationbar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
+      onTap: (value) => uiProvider.selectedMenuOpt = value,
       currentIndex: currentIndex,
       elevation: 0,
       items: <BottomNavigationBarItem>[
