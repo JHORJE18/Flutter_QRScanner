7c7cb364c08d7d6da48a93ae12dda492d56d27dc
Jorge <wiijlg@hotmail.com>
2021-04-07 11:36:40 +0200

    Mostrar Scans UI

--- lib/pages/listMap_page.dart
+++ lib/pages/listMap_page.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mapas Page'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(
+          Icons.map,
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text('iamiamdiaikdniadiasmn'),
+        subtitle: Text('ID: 1'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () => print('Abrir algo'),
+      ),
     );
   }
 }
